a837f9b57d2cd9444829bf89e178e0796dd0c579
fanur56 <ilasovf@gmail.com>
2022-07-12 19:28:23 +0300

    Created test for functions removeTodoList, addTodoList

--- src/reducers/todolist-reducer.test.ts
+++ src/reducers/todolist-reducer.test.ts
@@ -18,3 +18,22 @@ test('correct todolist should be removed', () => {
     expect(endState.length).toBe(1);
     expect(endState[0].id).toBe(todolistId1);
 });
+
+
+test('correct todolist should be added', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: Array<TodoListType> = [
+        {id: todolistId1, title: "What to learn", filter: "All"},
+        {id: todolistId2, title: "What to buy", filter: "All"}
+    ]
+
+    const endState = todolistsReducer(startState, {type: "ADD-TODOLIST", title: newTodolistTitle, id: v1()})
+
+    expect(endState.length).toBe(3);
+    expect(endState[2].title).toBe(newTodolistTitle);
+});
+
--- src/reducers/todolist-reducer.ts
+++ src/reducers/todolist-reducer.ts
@@ -1,14 +1,26 @@
 import {TodoListType} from "../App";
+import {v1} from "uuid";
 
 type RemoveTodoListAT = {
     type: "REMOVE-TODOLIST"
-    id:string
+    id: string
 }
 
-export const todolistsReducer = (todoLists: Array<TodoListType>, action: RemoveTodoListAT):Array<TodoListType> => {
+type AddTodoListAT = {
+    type: "ADD-TODOLIST"
+    title: string
+    id: string
+}
+
+type ActionType = RemoveTodoListAT | AddTodoListAT
+
+export const todolistsReducer = (todoLists: Array<TodoListType>, action: ActionType): Array<TodoListType> => {
     switch (action.type) {
         case "REMOVE-TODOLIST":
             return todoLists.filter(tl => tl.id !== action.id)
+        case "ADD-TODOLIST":
+            const newTodoListID = v1()
+            return [...todoLists, {id: action.id, title: action.title, filter: 'All'}]
         default:
             return todoLists
     }

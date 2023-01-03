ec59cd87c7cca300621555af4b2a968ac3066fa7
fanur56 <ilasovf@gmail.com>
2023-01-03 19:48:13 +0300

    Added to storybook DeleteTodolists, UpdateTodolists requests from server

--- src/stories/API/todolists-api.stories.tsx
+++ src/stories/API/todolists-api.stories.tsx
@@ -40,3 +40,31 @@ export const CreateTodolists = () => {
 
     return <div>{JSON.stringify(state)}</div>
 }
+
+export const DeleteTodolists = (props: any )=> {
+
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        instance.delete(`todo-lists/f85621ea-788e-41ad-9bfe-5174e4a8be48`)
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolists = () => {
+
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        instance.put("todo-lists/d6373953-3948-4366-808f-ed2e1a8932b8", {title: "Yo"})
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}

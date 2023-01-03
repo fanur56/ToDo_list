0cf9497f5779efb373bf827b2da7d72bdd62fd5f
fanur56 <ilasovf@gmail.com>
2023-01-03 18:21:03 +0300

    Created API stories to Storybook and added GetTodolists request from server

---
+++ src/stories/API/todolists-api.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+export default {
+    title: "API"
+}
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    headers:     {
+        "API-KEY": "5681cc7e-1c04-4d08-8e39-18a3f5b202e6"
+    }
+});
+
+export const GetTodolists = () => {
+
+    const [state, setState] = useState<any>(null)
+
+    useEffect(() => {
+        instance.get("todo-lists")
+            .then((res) => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}

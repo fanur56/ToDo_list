0848b5a38faee78e9f648aa5612a68fb2afad1da
fanur56 <ilasovf@gmail.com>
2023-01-04 12:11:03 +0300

    Created todolists-api.ts with CRUD methods

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,7 +1,24 @@
+import axios from "axios";
 
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    headers: {
+        "API-KEY": "5681cc7e-1c04-4d08-8e39-18a3f5b202e6"
+    }
+});
 
 export const todolistsAPI = {
     getTodolists() {
-
+       return instance.get("todo-lists")
+    },
+    postTodolist() {
+        return instance.post("todo-lists", {title: "List"})
+    },
+    deleteTodolist(){
+        return instance.delete(`todo-lists/f85621ea-788e-41ad-9bfe-5174e4a8be48`)
+    },
+    putTodolist(){
+        return instance.put("todo-lists/d6373953-3948-4366-808f-ed2e1a8932b8", {title: "Yo"})
     }
 }
--- src/stories/API/todolists-api.stories.tsx
+++ src/stories/API/todolists-api.stories.tsx
@@ -1,24 +1,16 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
+import {todolistsAPI} from "../../api/todolists-api";
 
 export default {
     title: "API"
 }
-
-const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    headers: {
-        "API-KEY": "5681cc7e-1c04-4d08-8e39-18a3f5b202e6"
-    }
-});
 
 export const GetTodolists = () => {
 
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        instance.get("todo-lists")
+        todolistsAPI.getTodolists()
             .then((res) => {
                 setState(res.data)
             })
@@ -32,7 +24,7 @@ export const CreateTodolists = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        instance.post("todo-lists", {title: "List"})
+        todolistsAPI.postTodolist()
             .then((res) => {
                 setState(res.data)
             })
@@ -41,12 +33,12 @@ export const CreateTodolists = () => {
     return <div>{JSON.stringify(state)}</div>
 }
 
-export const DeleteTodolists = (props: any )=> {
+export const DeleteTodolists = ()=> {
 
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        instance.delete(`todo-lists/f85621ea-788e-41ad-9bfe-5174e4a8be48`)
+        todolistsAPI.deleteTodolist()
             .then((res) => {
                 setState(res.data)
             })
@@ -60,7 +52,7 @@ export const UpdateTodolists = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        instance.put("todo-lists/d6373953-3948-4366-808f-ed2e1a8932b8", {title: "Yo"})
+        todolistsAPI.putTodolist()
             .then((res) => {
                 setState(res.data)
             })
